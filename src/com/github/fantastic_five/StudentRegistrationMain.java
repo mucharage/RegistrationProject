840fcafc998e3e26c7ef89748ab29caa5ff8fa72
oitsjustjose <stovallj1995@gmail.com>
2016-03-31 12:40:21 -0400

    Serious work :D

--- src/com/github/fantastic_five/StudentRegistrationMain.java
+++ src/com/github/fantastic_five/StudentRegistrationMain.java
@@ -4,8 +4,8 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
@@ -26,8 +26,6 @@ public class StudentRegistrationMain
 	public static JFrame mainWindow = new JFrame("FF Student Registration");
 	public static Dimension mainWindowDimension = new Dimension(618, 458);
 	public static ArrayList<UserProfile> loggedIn = new ArrayList<UserProfile>();
-	public static PrintWriter classOutput;
-	public static PrintWriter userOutput;
 
 	public static void main(String[] args)
 	{
@@ -46,50 +44,41 @@ public class StudentRegistrationMain
 			@Override
 			public void run()
 			{
-				// Put anything that needs to run in main here!
+				readInFromFiles();
 				createBaseUsers();
 				createMainWindow();
-				initializeOutFiles();
 				replaceMainWindowContents(new GUILogin());
 			}
 		};
 		SwingUtilities.invokeLater(r);
 	}
-	
-	private static void initializeOutFiles()
+
+	/**
+	 * initializes variables using data stored in our .dat files
+	 */
+	private static void readInFromFiles()
 	{
-		try
+		File coursesFile = new File("courses.dat");
+		if (coursesFile != null)
 		{
-			classOutput = new PrintWriter(new File("classes.dat"));
-			userOutput = new PrintWriter(new File("users.dat"));
-		}
-		catch (FileNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	public static void clearUserOutput()
-	{
-		try
-		{
-			userOutput = new PrintWriter(new File("class.dat"));
-		}
-		catch(FileNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	public static void clearClassOutput()
-	{
-		try
-		{
-			classOutput = new PrintWriter(new File("class.dat"));
-		}
-		catch(FileNotFoundException e)
-		{
-			e.printStackTrace();
+			try
+			{
+				Scanner courseIn = new Scanner(new File("courses.dat"));
+				while(courseIn.hasNextLine())
+				{
+					String line = courseIn.nextLine();
+					String[] lineParts = line.split(" | ");
+					String title = lineParts[0];
+					String description = lineParts[1];
+					int CRN = Integer.parseInt(lineParts[2]);
+					int cap = Integer.parseInt(lineParts[3]);
+					
+				}
+			}
+			catch (FileNotFoundException e)
+			{
+				e.printStackTrace();
+			}
 		}
 	}
 
@@ -98,7 +87,11 @@ public class StudentRegistrationMain
 	 */
 	private static void createBaseUsers()
 	{
+		UserProfile teacherUser = new UserProfile("teacher", "password", UserProfile.TEACHER, "Group", "Five", "Teacher");
+		UserProfile studentUser = new UserProfile("student", "password", UserProfile.STUDENT, "Group", "Five", "Student");
 		UserProfile adminUser = new UserProfile("admin", "password", UserProfile.ADMIN, "Group", "Five", "Administrator");
+		UserProfileDatabase.addUser(teacherUser);
+		UserProfileDatabase.addUser(studentUser);
 		UserProfileDatabase.addUser(adminUser);
 	}
 
