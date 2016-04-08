d63e628ac20cc23bb848b6af293db57bf3d904ce
sahullander <shifty50@ymail.com>
2016-04-07 21:57:01 -0400

    Revert "current Logged in changes"
    This reverts commit 14f8770f472c2baf1aab615b8d797ba2df18c7a9.

--- src/com/github/fantastic_five/GUIMisc/GUILogStatus.java
+++ src/com/github/fantastic_five/GUIMisc/GUILogStatus.java
@@ -1,6 +1,5 @@
 package com.github.fantastic_five.GUIMisc;
 
-import java.awt.Color;
 import java.awt.Frame;
 /**
  * @author Fantastic Five (Jose Stovall)
@@ -13,7 +12,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 
 import com.github.fantastic_five.StudentRegistrationMain;
 import com.github.fantastic_five.GUI.GUILogin;
@@ -66,34 +64,13 @@ public class GUILogStatus extends JPanel
 			}
 		});
 		add(btnLogOut);
+
 		UserProfile u = StudentRegistrationMain.loggedIn.get(0);
-		String currentUser = u.getUserID();
-		JButton btnCurrentUser = new JButton(currentUser);
-		if(u.getPermLevel() == 0){btnCurrentUser.setBounds(172, 1, 100, 20);}
-		else if(u.getPermLevel() == 1 || u.getPermLevel() == 3){btnCurrentUser.setBounds(172, 1, 100, 20);}
-		else if(u.getPermLevel() == 2){btnCurrentUser.setBounds(226, 1, 100, 20);}
-		else if(u.getPermLevel() == 4){btnCurrentUser.setBounds(163, 1, 100, 20);}
-		btnCurrentUser.setHorizontalAlignment(SwingConstants.LEFT);
-		btnCurrentUser.setContentAreaFilled(false);
-		btnCurrentUser.setBorderPainted(false);
-		btnCurrentUser.setForeground(Color.BLUE);
-		btnCurrentUser.addActionListener(new ActionListener()
-		{
-			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				{
-					//TODO implement what happens when user name is clicked
-				}
-			}});
-		add(btnCurrentUser);
-					
-		JLabel lblCurrentLoggedIn = new JLabel("Current Logged In User (" + getPermDescriptionFromInt(u.getPermLevel()) + "):");
+		JLabel lblCurrentLoggedIn = new JLabel("Current Logged In User: " + u.getFirstName() + " " + u.getLastName() + " (" + getPermDescriptionFromInt(u.getPermLevel()) + ")");
 		lblCurrentLoggedIn.setBounds(10, 4, 517, 14);
 		add(lblCurrentLoggedIn);
-		}
-		
-		
+	}
+
 	public String getPermDescriptionFromInt(int level)
 	{
 		switch (level)
