23d93b7c059bd12888edc71ff31e0d00dac5e310
oitsjustjose <stovallj1995@gmail.com>
2016-04-15 14:26:04 -0400

    Fully featuring add + remove courses!

--- src/com/github/fantastic_five/Logic/CourseManager.java
+++ src/com/github/fantastic_five/Logic/CourseManager.java
@@ -1,11 +1,9 @@
 package com.github.fantastic_five.Logic;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.io.PrintStream;
 import java.io.Serializable;
 import java.util.Comparator;
 import java.util.HashSet;
