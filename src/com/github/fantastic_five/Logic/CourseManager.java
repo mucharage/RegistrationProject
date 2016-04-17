f10f45eb8c937a917bba7f644903693c130c6a3b
unfallible <stephenclark314@gmail.com>
2016-04-17 19:53:08 -0400

    Minor bugfix to containsCourse method

--- src/com/github/fantastic_five/Logic/CourseManager.java
+++ src/com/github/fantastic_five/Logic/CourseManager.java
@@ -54,7 +54,7 @@ public class CourseManager implements Serializable
 	public boolean containsCourse(int crn)
 	{
 		Course dummy = dummyCourse(crn);
-		return courseOfferings.floor(dummy).equals(dummy);
+		return courseOfferings.contains(dummy);
 	}
 
 	/**
