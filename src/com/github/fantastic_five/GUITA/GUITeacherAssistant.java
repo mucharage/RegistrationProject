22be06b6bc0c7dfd12a6b5b090b37805e2aabc54
oitsjustjose <stovallj1995@gmail.com>
2016-04-20 20:17:40 -0400

    Teacher Assistant GUI Overhaul

--- src/com/github/fantastic_five/GUITA/GUITeacherAssistant.java
+++ src/com/github/fantastic_five/GUITA/GUITeacherAssistant.java
@@ -1,21 +1,10 @@
 package com.github.fantastic_five.GUITA;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Frame;
-import java.awt.Point;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+import javax.swing.JTabbedPane;
 
-import com.github.fantastic_five.StudentRegistrationMain;
-import com.github.fantastic_five.GUIMisc.GUILogStatus;
 import com.github.fantastic_five.GUIStudent.GUIStudent;
 import com.github.fantastic_five.GUITeacher.GUITeacher;
 
@@ -24,112 +13,17 @@ public class GUITeacherAssistant extends JPanel
 {
 	public GUITeacherAssistant()
 	{
+		setBounds(0, 0, 568, 391);
 		setLayout(null);
-		setBounds(0, 0, 618, 434);
 
-		// Adds the login panel to this window
-		JPanel loginPanel = new GUILogStatus();
-		loginPanel.setBounds(0, 0, 618, 24);
-		add(loginPanel);
-
-		JButton btnTeacherView = new JButton("Teacher View");
-		btnTeacherView.setBounds(223, 168, 177, 23);
-		btnTeacherView.addActionListener(new ActionListener()
-		{
-			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				if (!isTeacherWindowOpen())
-					createTeacherWindow();
-			}
-		});
-		add(btnTeacherView);
-
-		JButton btnStudentView = new JButton("Student View");
-		btnStudentView.setBounds(223, 202, 177, 23);
-		btnStudentView.addActionListener(new ActionListener()
-		{
-			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				if (!isStudentWindowOpen())
-					createStudentWindow();
-			}
-		});
-		add(btnStudentView);
-
-		// Panel label
-		JLabel lblAdministration = new JLabel("Teacher's Assistant");
-		lblAdministration.setForeground(Color.GRAY);
-		lblAdministration.setFont(new Font("Verdana", Font.BOLD, 16));
-		lblAdministration.setHorizontalAlignment(SwingConstants.CENTER);
-		lblAdministration.setBounds(191, 97, 243, 23);
-		add(lblAdministration);
-	}
-
-	/**
-	 * @return true if the Student Perspective window is open, false otherwise
-	 */
-	boolean isStudentWindowOpen()
-	{
-		Frame[] allFrames = Frame.getFrames();
-		for (Frame f : allFrames)
-			if (f instanceof JFrame)
-				if (f.getTitle() == "Student Perspective")
-					return true;
-
-		return false;
-	}
-
-	/**
-	 * @return true if the Teacher Perspective window is open, false otherwise
-	 */
-	boolean isTeacherWindowOpen()
-	{
-		Frame[] allFrames = Frame.getFrames();
-		for (Frame f : allFrames)
-			if (f instanceof JFrame)
-				if (f.getTitle() == "Teacher Perspective")
-					return true;
-
-		return false;
-	}
-
-	public void createStudentWindow()
-	{
-		// Makes a new window
-		JFrame studentView = new JFrame("Student Perspective");
-		// Finds a starting point for it to be put at
-		Point startingLoc = StudentRegistrationMain.mainWindow.getLocation();
-		startingLoc.translate(-40, 30);
-
-		// Basic pop-up window initialization
-		studentView.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		studentView.setPreferredSize(new Dimension(618, 458));
-		studentView.setResizable(false);
-		studentView.pack();
-		studentView.setVisible(true);
-		studentView.setLocation(startingLoc);
-		studentView.getContentPane().add(new GUIStudent());
-		studentView.pack();
-	}
-
-	public void createTeacherWindow()
-	{
-		// Makes a new window
-		JFrame teacherView = new JFrame("Teacher Perspective");
-		// Finds a starting point for it to be put at
-		Point startingLoc = StudentRegistrationMain.mainWindow.getLocation();
-		startingLoc.translate(40, 20);
-
-		// Basic pop-up window initialization
-		teacherView.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		teacherView.setPreferredSize(new Dimension(618, 458));
-		teacherView.setResizable(false);
-		teacherView.pack();
-		teacherView.setVisible(true);
-		teacherView.setLocation(startingLoc);
-		teacherView.getContentPane().add(new GUITeacher());
-		teacherView.pack();
+		JTabbedPane tabbedPane = new JTabbedPane();
+		tabbedPane.setBounds(0, 0, 618, 434);
+		tabbedPane.addTab("Student", new GUIStudent());
+		tabbedPane.setMnemonicAt(0, KeyEvent.VK_2);
+		
+		add(tabbedPane);
+		GUITeacher teacher = new GUITeacher();
+		tabbedPane.addTab("Teacher", teacher);
+		tabbedPane.setMnemonicAt(1, KeyEvent.VK_1);
 	}
 }
