a0968577c32c0ad71532c2e6693e8f6249b7af1a
6cphillips <6cphillips@gmail.com>
2016-04-23 16:11:57 -0400

    Update GUIViewStudent.java

--- src/com/github/fantastic_five/GUITeacher/GUIViewStudent.java
+++ src/com/github/fantastic_five/GUITeacher/GUIViewStudent.java
@@ -116,25 +116,6 @@ public class GUIViewStudent extends JPanel
 		lblViewSchedule.setHorizontalAlignment(SwingConstants.CENTER);
 		add(lblViewSchedule);
 		
-		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(10, 179, 598, 196);
-		add(scrollPane_1);
-		
-		table_1 = new JTable();
-		table_1.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-			},
-			new String[] {
-				"User ID", "Last Name", "First Name"
-			}
-		));
-		scrollPane_1.setViewportView(table_1);
-		
 		/**
 		 * Displays currently enrolled students by double clicking selected course
 		 */
@@ -148,58 +129,44 @@ public class GUIViewStudent extends JPanel
 					
 					if (rowSel > -1)
 					{
-						StudentRegistrationMain.mainCourseManager.getLearnersWithCourse((int) addedTable.getModel().getValueAt(rowSel, 0));
-						table_1.setModel(new DefaultTableModel(getStudentTable(), new String[] { "User ID", "Last Name", "First Name" })
-						{
-							@Override
-							public boolean isCellEditable(int row, int column)
-							{
-								return false;
-							}
-						});
-						scrollPane_1.setViewportView(table_1);
-						revalidate();
-						repaint();	
-						//System.out.println(StudentRegistrationMain.mainCourseManager.getLearnersWithCourse((int) addedTable.getModel().getValueAt(rowSel, 0)));
-						
 						Course selectedCourse = StudentRegistrationMain.mainCourseManager.getCourse((int) addedTable.getModel().getValueAt(addedTable.getSelectedRow(), 0));
 						Set<UserProfile> student = StudentRegistrationMain.mainCourseManager.getLearnersWithCourse(selectedCourse.getCRN());
 						
-						Iterator<UserProfile> names = student.iterator();
+						JDialog popup = new JDialog(StudentRegistrationMain.mainWindow, selectedCourse.getTitle() + " - Students");
+						popup.setBounds(200, 200, 447, 147);
+						popup.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+						popup.setLocationRelativeTo(StudentRegistrationMain.mainWindow);
+						popup.setResizable(false);
+						popup.setVisible(true);
+						popup.setAlwaysOnTop(true);
+						
+						JTextArea desc = new JTextArea();
+						Iterator<UserProfile> names = student.iterator();		
+						String x = names.toString();
 						while(names.hasNext())
-						{
+						{									
+					
+							
 							System.out.println(names.next());
 						}
-					
+						
+						//JTextArea desc = new JTextArea();
+						desc.setText(selectedCourse.getDescription());
+						desc.setText(x);
+						desc.setWrapStyleWord(true);
+						desc.setLineWrap(true);
+						desc.setFont(new Font("Verdana", Font.PLAIN, 12));
+						desc.setBounds(10, 11, 421, 96);						
+						desc.setEditable(false);
+						scrollPane.setViewportView(desc);
+						
+						JScrollPane scrollPane = new JScrollPane(desc);	
+						scrollPane.setBounds(10, 11, 421, 96);
+						popup.getContentPane().add(scrollPane);		
+						
 					}
 				}
 			}
 		});
-	}	
-	
-	/**
-	 * @return fills or removes information from the table when a student adds or drops the selected course.
-	 */
-	public static Object[][] getStudentTable()
-	{
-		Set<Course> selectedCourse = StudentRegistrationMain.mainCourseManager.getCoursesWithInstructor(StudentRegistrationMain.getCurrentLoggedInUser());
-		Object[][] cells = new Object[selectedCourse.size()][3];
-		int row = 0;		
-		for (Course c : selectedCourse)
-		{
-			Set<UserProfile> student = StudentRegistrationMain.mainCourseManager.getLearnersWithCourse(c.getCRN());
-			
-			Iterator<UserProfile> names = student.iterator();
-			while(names.hasNext())
-			{
-				System.out.println(names.next());
-			}
-			//student.getUserID();
-			//cells[row][0] = c.getUserID();
-			cells[row][1] = c.getTitle();
-			cells[row][2] = c.getStartTime(Course.TWENTYFOUR_HR_CLOCK) + "-" + c.getEndTime(Course.TWENTYFOUR_HR_CLOCK);
-			row++;
-		}
-		return cells;
 	}
 }
