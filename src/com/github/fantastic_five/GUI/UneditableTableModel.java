77bd2ec2a26c7a9c862a1383422707685b6d5bc4
oitsjustjose <stovallj1995@gmail.com>
2016-04-23 10:19:23 -0400

    Check for pre-existing pending application

--- src/com/github/fantastic_five/GUI/UneditableTableModel.java
+++ src/com/github/fantastic_five/GUI/UneditableTableModel.java
@@ -1,5 +1,10 @@
 package com.github.fantastic_five.GUI;
 
+/**
+ * @author Fantastic Five (Jose Stovall)
+ * This is a child class of DefaultTableModel that does not allow cell editing. Avoids constant creation of anonymous classes
+ */
+
 import javax.swing.table.DefaultTableModel;
 
 @SuppressWarnings("serial")
